bdde3c1735d9fbd322657b25aad5a4061110b146
leoosilveira80 <42881099+leoosilveira80@users.noreply.github.com>
2023-08-31 10:22:35 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,27 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-# Load your data
-df = pd.read_excel('sample_product_costs.xlsx')
 
 # Page title
-st.title('Product Cost Analysis Dashboard')
+st.title('Simple Streamlit App')
 
-# Sidebar for selecting a product
-selected_product = st.sidebar.selectbox('Select a Product', df['Product'].unique())
+# Input field for user name
+user_name = st.text_input('Enter your name', 'Your Name')
 
-# Filter the data based on the selected product
-filtered_df = df[df['Product'] == selected_product]
+# Display user input
+st.write(f'Hello, {user_name}!')
 
-# Show data summary
-st.write(f"## Data Summary for {selected_product}")
-st.write(filtered_df)
+# Slider for selecting a number
+selected_number = st.slider('Select a number', 1, 100, 50)
+st.write(f'You selected: {selected_number}')
 
-# Show scatter plot
-st.write(f"## Cost Analysis for {selected_product}")
-scatter_fig = px.scatter(filtered_df, x='Month', y='Cost', color='Category', title=f'Cost Analysis for {selected_product}')
-st.plotly_chart(scatter_fig)
+# Button to do something
+if st.button('Click me'):
+    st.write('Button clicked!')
 
-# Show bar chart for cost percentage by category
-st.write(f"## Cost Percentage by Category for {selected_product}")
-total_cost_by_month = filtered_df.groupby(['Month'])['Cost'].sum().reset_index()
-filtered_df = filtered_df.merge(total_cost_by_month, on='Month', suffixes=('', '_total'))
-filtered_df['Cost_Percentage'] = (filtered_df['Cost'] / filtered_df['Cost_total']) * 100
-bar_fig = px.bar(filtered_df, x='Month', y='Cost_Percentage', color='Category',
-                 title=f'Cost Percentage by Category for {selected_product}', range_y=[0, 100])
-st.plotly_chart(bar_fig)
+# Checkbox
+if st.checkbox('Show details'):
+    st.write('Some additional details here.')
+
+# Selectbox
+options = ['Option 1', 'Option 2', 'Option 3']
+selected_option = st.selectbox('Select an option', options)
+st.write(f'Selected option: {selected_option}')
