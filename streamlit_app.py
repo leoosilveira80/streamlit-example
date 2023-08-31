b3d76d45708fa8eda8af38bf27a39a558d3796ca
leoosilveira80 <42881099+leoosilveira80@users.noreply.github.com>
2023-08-31 10:26:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,36 @@ if st.checkbox('Show details'):
 options = ['Option 1', 'Option 2', 'Option 3']
 selected_option = st.selectbox('Select an option', options)
 st.write(f'Selected option: {selected_option}')
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Set page title
+st.title('Excel Data Analysis with Streamlit')
+
+# Upload Excel file
+uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx", "xls"])
+
+if uploaded_file is not None:
+    # Load Excel data
+    df = pd.read_excel(uploaded_file)
+
+    # Display data
+    st.subheader('Data Preview')
+    st.write(df)
+
+    # Create a scatter plot
+    st.subheader('Scatter Plot')
+    scatter_fig = px.scatter(df, x='Month', y='Cost', color='Category', title='Cost Analysis')
+    st.plotly_chart(scatter_fig)
+
+    # Create a bar chart for cost percentage by category
+    st.subheader('Cost Percentage by Category')
+    total_cost_by_month = df.groupby(['Month'])['Cost'].sum().reset_index()
+    df = df.merge(total_cost_by_month, on='Month', suffixes=('', '_total'))
+    df['Cost_Percentage'] = (df['Cost'] / df['Cost_total']) * 100
+    bar_fig = px.bar(df, x='Month', y='Cost_Percentage', color='Category',
+                     title='Cost Percentage by Category', range_y=[0, 100])
+    st.plotly_chart(bar_fig)
+
