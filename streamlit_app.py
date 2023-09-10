d5ef620044c8dcd04712f81d55de9d07cf0341f2
leoosilveira80 <42881099+leoosilveira80@users.noreply.github.com>
2023-09-09 21:53:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,3 +98,30 @@ pie_chart = px.pie(df_participants,
                 names='Departments')
 
 st.plotly_chart(pie_chart)
+
+import streamlit as st
+import cv2
+import numpy as np
+
+def compare_images(img1, img2):
+    # Load the images
+    img1 = cv2.imread(img1, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(img2, cv2.IMREAD_GRAYSCALE)
+
+    # Calculate the absolute difference between the images
+    diff = cv2.absdiff(img1, img2)
+
+    # Threshold the difference image to highlight the differences
+    _, diff = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Save the difference image
+    cv2.imwrite("difference.tif", diff)
+
+st.title("TIFF Image Comparison")
+
+img1 = st.file_uploader("Upload File 1 (TIFF)", type=["tif"])
+img2 = st.file_uploader("Upload File 2 (TIFF)", type=["tif"])
+
+if img1 is not None and img2 is not None:
+    compare_images(img1, img2)
+    st.image("difference.tif", caption="Difference Image", use_column_width=True)
